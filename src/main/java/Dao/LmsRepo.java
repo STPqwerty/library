445ebf45ecb5327fd159f18f17b22c2978393088
Alex <aleksandrstp1>
2018-11-01 23:59:31 +0300

    v1 (jsp doesn't work)

---
+++ src/main/java/Dao/LmsRepo.java
@@ -0,0 +1,8 @@
+package Dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LmsRepo extends CrudRepository {
+}
