590ab78bf0007a1cab642485038696be58e3f6ba
Alex <aleksandrstp1>
2018-11-01 23:08:51 +0300

    v1 (jsp doesn't work)

---
+++ src/main/java/lms/library/MainController.java
@@ -0,0 +1,13 @@
+package lms.library;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/helloJsp")
+    public String init(){
+        return "index";
+    }
+}
